4a63918ed52a23a6968b51a0071e7d5f0ffd3f45
Hadjati Darius <hadjati.darius90@gmail.com>
2023-06-14 12:38:55 +0200

    init page + creating the drag and drop functionality

---
+++ app.js
@@ -0,0 +1,78 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+
+
+
+// … 
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        }
+        else {
+            container.insertBefore(draggable, afterElement)
+        }
+    })
+})
+
+
+// … 
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        // pauseEvent(e);
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        }
+        else {
+            container.insertBefore(draggable, afterElement)
+        }
+    })
+})
+
+
+
+// …
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+
+    // …
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        }
+        else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+function pauseEvent(e) {
+    if (e.stopPropagation) e.stopPropagation();
+    if (e.preventDefault) e.preventDefault();
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+}
+
+
+
