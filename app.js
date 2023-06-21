7eac98a0291ea2d7462429e29d9f3f80391bfbef
Hadjati Darius <hadjati.darius90@gmail.com>
2023-06-21 16:28:11 +0200

    cleaning the code: adding comments to the code and modules

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const columns = document.querySelectorAll('.col')
 const btnLeft = document.querySelector('.btn-left')
 const btnRight = document.querySelector('.btn-right')
 const btnDelete = document.querySelector('.btn-delete')
-const btnNew = document.querySelector('.btn-new')
 const btnOpenForm = document.querySelector('.open-button')
 const btnCancelForm = document.querySelector('button.cancel')
 const btnAddForm = document.querySelector('button.add')
+
+import { addEventListeners } from "./events.js"
 
 class Task {
     constructor(text, state) {
@@ -15,38 +16,19 @@ class Task {
     }
 }
 
-function addEventListeners(element) {
-    element.addEventListener('dragstart', () => {
-        element.classList.add('dragging');
-    })
 
-    element.addEventListener('dragend', () => {
-        element.classList.remove('dragging')
-    })
-
-    element.addEventListener('click', () => {
-        let prevFocus = document.querySelector('.focus')
-        element.classList.toggle('focus')
-        if (prevFocus !== element) prevFocus.classList.remove('focus');
-    })
-
-    element.addEventListener('dblclick', () => {
-        let newTaskText = prompt('Rename task:');
-        if (newTaskText) element.firstElementChild.firstElementChild.textContent = newTaskText;
-    })
-}
-
-
+//Apply the event listeners to all draggable elements
 draggables.forEach(draggable => {
     addEventListeners(draggable)
 })
 
 
-// …
+// Call a reduce function which will loop through the list of draggable elements and also specify the single element after the mouse cursor.
+// Return the reduce function by adding the first element as closest and the second as a child. Also, equate the offset and add conditions.
 function getDragAfterElement(column, y) {
     const draggableElements = [...column.querySelectorAll('[draggable=true]:not(.dragging)')]
 
-    // …
+
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
         const offset = y - box.top - box.height / 2
@@ -59,18 +41,17 @@ function getDragAfterElement(column, y) {
     }, { offset: Number.NEGATIVE_INFINITY }).element
 }
 
-
-// … 
+//add dragover event for hovering elements around
 columns.forEach(column => {
     column.addEventListener('dragover', e => {
         e.preventDefault()
         const afterElement = getDragAfterElement(column, e.clientY)
-        const draggable = document.querySelector('.dragging')
-        if (afterElement == null) {
-            column.appendChild(draggable)
+        const draggable = document.querySelector('.dragging') //set the current draggable element then append it to the current container
+        if (afterElement == null) {                         // set conditions and check the AfterElements, 
+            column.appendChild(draggable)                      //and if it is set to null, append a child at the end of the list.
         }
         else {
-            column.insertBefore(draggable, afterElement)
+            column.insertBefore(draggable, afterElement)     // Else, add the element draggable and afterElement as parameters in the insertBefore function.
         }
     })
 })
@@ -154,8 +135,6 @@ function addCard(task, element) {
     }
 
 }
-
-
 
 btnAddForm.addEventListener('click', (e) => {
     e.preventDefault();
---
+++ events.js
@@ -0,0 +1,22 @@
+//Add all the event listeners to the card element
+export function addEventListeners(element) {
+    element.addEventListener('dragstart', () => {
+        element.classList.add('dragging');  //Add dragging class when dragstart
+    })
+
+    element.addEventListener('dragend', () => {
+        element.classList.remove('dragging') //Remove dragging class when dragend
+    })
+
+    element.addEventListener('click', () => {
+        let prevFocus = document.querySelector('.focus')
+        element.classList.toggle('focus')  //Toogle focus class on clicked element
+        if (prevFocus !== element) prevFocus.classList.remove('focus'); //If there is already a focus class element, then compare 
+        //it, and if it is the same then remove the focus class instead of toggle
+    })
+
+    element.addEventListener('dblclick', () => {
+        let newTaskText = prompt('Rename task:');
+        if (newTaskText) element.firstElementChild.firstElementChild.textContent = newTaskText;
+    })
+}
